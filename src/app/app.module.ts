5810eb84bd1f076ccba559fac0c9c6004fece400
MalindaK <kumarasinghe@axinom.com>
2020-05-19 09:14:09 +0530

    Added initial files realted to angular project

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DataTableComponent } from './components/data-table/data-table.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { ResultsChartComponent } from './components/results-chart/results-chart.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DataTableComponent,
+    FilterComponent,
+    ResultsChartComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/results-chart/results-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-results-chart',
+  templateUrl: './results-chart.component.html',
+  styleUrls: ['./results-chart.component.scss']
+})
+export class ResultsChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
