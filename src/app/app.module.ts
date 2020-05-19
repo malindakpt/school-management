78f191a2aa877df560bcd210dc43aad213ffb5f7
MalindaK <kumarasinghe@axinom.com>
2020-05-19 12:25:40 +0530

    Added basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DataTableComponent } from './components/data-table/data-table.component';
-import { FilterComponent } from './components/filter/filter.component';
-import { ResultsChartComponent } from './components/results-chart/results-chart.component';
+import { HeaderComponent } from './components/containerComponents/header/header.component';
+import { LoginComponent } from './components/containerComponents/login/login.component';
+import { DataTableComponent } from './components/presentationComponents/data-table/data-table.component';
+import { FilterComponent } from './components/presentationComponents/filter/filter.component';
+import { ResultsChartComponent } from './components/presentationComponents/results-chart/results-chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DataTableComponent,
     FilterComponent,
-    ResultsChartComponent
+    ResultsChartComponent,
+    HeaderComponent,
+    LoginComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
