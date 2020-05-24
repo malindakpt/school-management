d89d1c887d1eed228123a3b2c8b3f60dc0de7f21
MalindaK <kumarasinghe@axinom.com>
2020-05-24 12:09:43 +0530

    Ehanced filter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { BaseComponent } from './components/presentationComponents/base/base.com
 import { MaterialModule } from './modules/app.material.module';
 import { HttpClientModule } from '@angular/common/http';
 import { DataService } from './services/data.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { DataService } from './services/data.service';
     BaseComponent,
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
--- src/app/components/presentationComponents/filter/filter.component.ts
+++ src/app/components/presentationComponents/filter/filter.component.ts
@@ -1,15 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  EventEmitter,
+  Component,
+  Input,
+  OnChanges,
+  Output,
+} from '@angular/core';
+import { Class } from 'src/app/entities/class';
 
+export interface FilterObj {
+  class?: string;
+  student?: string;
+  fromDate?: Date;
+  toDate?: Date;
+}
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
-  styleUrls: ['./filter.component.scss']
+  styleUrls: ['./filter.component.scss'],
 })
-export class FilterComponent implements OnInit {
+export class FilterComponent implements OnChanges {
+  @Input() classes: Class[];
+  @Output() filterChange = new EventEmitter();
 
-  constructor() { }
+  public viewClasses: string[];
+  public viewStudents: string[];
 
-  ngOnInit(): void {
+  public selectedClass;
+  public selectedStudent;
+  public selectedStartDate;
+  public selectedEndDate;
+
+  private classMap = {};
+
+  ngOnChanges(): void {
+    const viewClasses = [undefined];
+    this.classes?.map((cls) => {
+      viewClasses.push(cls.name);
+
+      if (!this.classMap[cls.name]) {
+        this.classMap[cls.name] = [undefined];
+      }
+      this.classMap[cls.name].push(cls.students);
+    });
+    this.viewClasses = viewClasses;
   }
 
+  public handleStartDateChange(e: any): void {
+    this.selectedStartDate = e.target.value;
+    this.filtersChanged();
+  }
+
+  public handleEndDateChange(e: any): void {
+    this.selectedEndDate = e.target.value;
+    this.filtersChanged();
+  }
+
+  public handleClassChange(e: any): void {
+    this.selectedClass = e.value;
+    this.viewStudents = this.classMap[e.value];
+    this.filtersChanged();
+  }
+
+  public handleStudentChange(e: any): void {
+    this.selectedStudent = e.value;
+    this.filtersChanged();
+  }
+
+  public filtersChanged(): void {
+    const filterObj: FilterObj = {
+      class: this.selectedClass,
+      student: this.selectedStudent,
+      fromDate: this.selectedStartDate,
+      toDate: this.selectedEndDate,
+    };
+    this.filterChange.emit(filterObj);
+  }
 }
