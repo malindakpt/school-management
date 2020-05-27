c164b603008b2bb2a1802a94e222b35d9dddb19f
MalindaK <kumarasinghe@axinom.com>
2020-05-27 18:35:17 +0530

    Reactored unit tests for filter comp

--- src/app/components/filter/filter.component.spec.ts
+++ src/app/components/filter/filter.component.spec.ts
@@ -7,13 +7,11 @@ import { Class } from 'src/app/interfaces/class';
 import { DataStore } from 'src/app/services/data-store.service';
 
 class MockDataStore {
-  private activities: Activity[];
-  private classes: Class[] = [{ name: 'class_1', students: ['student_1'] }];
-
-  public setData(
-    activityResponse: ActivityResponse,
-    classesResponse: ClassesResponse
-  ): void {}
+  public activities: Activity[];
+  public classes: Class[] = [
+    { name: 'class_1', students: ['student_1'] },
+    { name: 'class_2', students: ['student_2'] },
+  ];
 
   public getActivities(): Activity[] {
     return this.activities;
@@ -22,51 +20,45 @@ class MockDataStore {
   public getClasses(): Class[] {
     return this.classes;
   }
-
-  // private getDate(dat: string): Date {
-  //   // Convert week into standard format and return as Date object
-  //   const arr = dat.split('/');
-  //   return new Date(`${arr[1]}/${arr[0]}/${arr[2]}`);
-  // }
 }
 
 describe('FilterComponent', () => {
   let comp: FilterComponent;
   let dataStore: DataStore;
-  // let fixture: ComponentFixture<FilterComponent>;
+  let mockDataStore: MockDataStore;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      // provide the component-under-test and dependent service
       providers: [
         FilterComponent,
         { provide: DataStore, useClass: MockDataStore },
       ],
     });
-    // inject both the component and the dependent service.
     comp = TestBed.inject(FilterComponent);
     dataStore = TestBed.inject(DataStore);
+    mockDataStore = new MockDataStore();
   });
 
-  it('should create', () => {
+  it('should be instantiated', () => {
     expect(comp).toBeTruthy();
   });
 
-  it('shoult have classes', () => {
-    const arr = [undefined, 'class_1'];
+  it('should generatecdefault Class array', () => {
+    const arr = [undefined, ...mockDataStore.classes.map((cls) => cls.name)];
     expect(comp.viewClasses).toEqual(arr);
   });
 
-  it('shoult have students', () => {
+  it('should generate default Student array as empty', () => {
     const arr = [undefined, 'class_1'];
     expect(comp.viewStudents).toEqual(undefined);
   });
 
-  it('handle class change', () => {
+  it('handle class change and generate students list', () => {
     spyOn(comp.filterChange, 'emit');
     comp.handleClassChange({ value: 'class_1' });
-    expect(comp.viewStudents).toEqual([undefined, 'student_1']);
 
+    expect(comp.selectedClass).toEqual('class_1');
+    expect(comp.viewStudents).toEqual([undefined, 'student_1']);
     expect(comp.filterChange.emit).toHaveBeenCalledWith({
       class: 'class_1',
       student: undefined,
@@ -78,6 +70,7 @@ describe('FilterComponent', () => {
   it('handle student change', () => {
     spyOn(comp.filterChange, 'emit');
     comp.handleStudentChange({ value: 'student_1' });
+
     expect(comp.selectedStudent).toEqual('student_1');
     expect(comp.filterChange.emit).toHaveBeenCalledWith({
       class: undefined,
@@ -90,6 +83,7 @@ describe('FilterComponent', () => {
   it('handle start date change', () => {
     spyOn(comp.filterChange, 'emit');
     comp.handleStartDateChange({ target: { value: '01/01/2020' } });
+
     expect(comp.selectedStartDate).toEqual('01/01/2020');
     expect(comp.filterChange.emit).toHaveBeenCalledWith({
       class: undefined,
@@ -102,6 +96,7 @@ describe('FilterComponent', () => {
   it('handle end date change', () => {
     spyOn(comp.filterChange, 'emit');
     comp.handleEndDateChange({ target: { value: '01/01/2020' } });
+
     expect(comp.selectedEndDate).toEqual('01/01/2020');
     expect(comp.filterChange.emit).toHaveBeenCalledWith({
       class: undefined,
